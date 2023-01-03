13817c96d01b4ab73297e8735062e827844f0ef0
ekplesovskaya <ekplesovskaya@gmail.com>
2023-01-03 16:13:25 +0300

    Initial commit

--- examples/control_example_gm.py
+++ examples/control_example_gm.py
@@ -7,7 +7,7 @@ def run_example():
     A control test which outputs stats_kde_cv_ml as the optimal generative model with the zu metric value of 1.056
     """
     X = load_iris().data
-    clf = GenerativeModel(num_syn_samples=10, hyperopt_time=0)
+    clf = GenerativeModel(num_syn_samples=10)
     clf.fit(X)
 
 
