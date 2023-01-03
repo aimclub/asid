73bb9da94e059e04f381fbb8dc3d7823a3df08f3
ekplesovskaya <ekplesovskaya@gmail.com>
2023-01-03 16:32:38 +0300

    Initial commit

---
+++ examples/control_example_abb.py
@@ -0,0 +1,25 @@
+from asid.automl_imbalanced.abb import AutoBalanceBoost
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
+
+
+def run_example():
+    # Create a synthetic dataset
+    X, Y = make_classification(n_classes=4, n_features=6, n_redundant=2, n_repeated=0, n_informative=4,
+                               n_clusters_per_class=2, flip_y=0.05, n_samples=700, random_state=45,
+                               weights=(0.7, 0.2, 0.05, 0.05))
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+    # Fit AutoBalanceBoost and evaluate its performance on synthetic dataset
+    print("AutoBalanceBoost")
+    clf = AutoBalanceBoost()
+    clf.fit(X_train, y_train)
+    pred = clf.predict(X_test)
+    print("AutoBalanceBoost F1-score for synthetic example: ", round(f1_score(y_test, pred, average="macro"), 2))
+    # Calculate feature importances of AutoBalanceBoost
+    feat_imp = clf.feature_importances()
+    print("Feature importances: ", feat_imp)
+
+
+if __name__ == "__main__":
+    run_example()
---
+++ examples/control_example_ilc.py
@@ -0,0 +1,15 @@
+from asid.automl_imbalanced.ilc import ImbalancedLearningClassifier
+from sklearn.datasets import make_classification
+
+
+def run_example():
+    # Create a synthetic dataset
+    X, Y = make_classification(n_classes=4, n_features=6, n_redundant=2, n_repeated=0, n_informative=4,
+                               n_clusters_per_class=2, flip_y=0.05, n_samples=700, random_state=45,
+                               weights=(0.7, 0.2, 0.05, 0.05))
+    clf = ImbalancedLearningClassifier(split_num=1)
+    clf.fit(X, Y)
+
+
+if __name__ == "__main__":
+    run_example()
