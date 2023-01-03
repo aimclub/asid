a223d52cf8817183923703a1d7714f1e16f25a39
ekplesovskaya <ekplesovskaya@gmail.com>
2023-01-03 16:01:58 +0300

    Initial commit

---
+++ examples/example_small_control.py
@@ -0,0 +1,15 @@
+from asid.automl_small.gm import GenerativeModel
+from sklearn.datasets import load_iris
+
+
+def run_example():
+    """
+    A control test which outputs stats_kde_cv_ml as the optimal generative model with the zu metric value of 1.056
+    """
+    X = load_iris().data
+    clf = GenerativeModel(num_syn_samples=10, hyperopt_time=0)
+    clf.fit(X)
+
+
+if __name__ == "__main__":
+    run_example()
