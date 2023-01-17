092a09d26a90880320bc191e80980c6c213ee37b
ekplesovskaya <ekplesovskaya@gmail.com>
2023-01-17 18:14:06 +0300

    Initial commit

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='asid',
     version=asid.version,
     author='Ekaterina',
-    long_description=open(Path(__file__).parent / 'README.md').read(),
+    long_description=open(Path(__file__).parent / 'README_en.md').read(),
     packages=find_packages(),
     test_suite='tests',
     install_requires=[],
