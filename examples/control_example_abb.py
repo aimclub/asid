97c60868c19d84d73871df99d4e89dcc31b7b094
ekplesovskaya <ekplesovskaya@gmail.com>
2023-01-19 13:13:39 +0300

    Initial commit

--- examples/control_example_abb.py
+++ examples/control_example_abb.py
@@ -5,6 +5,9 @@ from sklearn.metrics import f1_score
 
 
 def run_example():
+    """
+    A control test which outputs F1-score value of 0.6
+    """
     # Create a synthetic dataset
     X, Y = make_classification(n_classes=4, n_features=6, n_redundant=2, n_repeated=0, n_informative=4,
                                n_clusters_per_class=2, flip_y=0.05, n_samples=700, random_state=45,
